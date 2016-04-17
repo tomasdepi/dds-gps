716a4e597c5bc8e159735ef51c1d1b03628e699b
tomasdepi <tomas94depi@gmail.com>
2016-04-16 22:31:24 -0300

    cargo lo basico

---
+++ src/main/java/dds/dds_gps/App.java
@@ -0,0 +1,13 @@
+package dds.dds_gps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
